29eb346ac86ce622e3ef0d09c2c81e5478ab9509
tom <tom.erez8@gmail.com>
2018-09-11 16:07:04 +0300

    changed it from functions to class

--- Control/drone_communication.py
+++ Control/drone_communication.py
@@ -3,30 +3,32 @@ import socket
 from General.general_common import States
 from .control_common import AxisIndex
 
-# TODO: read these values from config file
-UDP_IP = "172.16.10.1"
-UDP_PORT = 8080
-BUFFER_SIZE = 1024
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
+class CommandCenter():
+    def __init__(self):
+        self.UDP_IP = "172.16.10.1" # TODO: read these values from config file
+        self.UDP_PORT = 8080
+        self.BUFFER_SIZE = 1024
+        self.sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
 
-def byte9(mSpeedValue,m360RollValue,mNoHeadValue,mStopValue,mToflyValue,mToLandValue):
-    return mSpeedValue | m360RollValue<<2 | mNoHeadValue<<4 | mStopValue<<5 | mToflyValue<<6 | mToLandValue<<7
+    def __send_to_drone(self,up_down, rotate, foward_backwards, left_right, byte6, byte7, byte8, byte9):
+        command = bytearray([255, 8, up_down, rotate, foward_backwards, left_right, byte6, byte7, byte8, byte9])
+        command.extend([255 - sum(command[1:]) % 256])  # check sum
+        self.sock.sendto(command, (self.UDP_IP, self.UDP_PORT))
 
-def send_command(up_down, rotate, foward_backwards, left_right, byte6, byte7, byte8, byte9):
-    command = bytearray([255,8,up_down,rotate,foward_backwards,left_right,byte6,byte7,byte8,byte9])
-    command.extend([255 - sum(command[1:]) % 256]) # check sum
-    sock.sendto(command, (UDP_IP, UDP_PORT))
+    def send_command(self,mode,my_joystick):
+        if mode == States.IDLE:
+            self.__send_to_drone(0, 63, 64, 63, 144, 16, 16, 0)  # stand by
+        elif mode == States.STAND_BY:
+            self.__send_to_drone(126, 63, 64, 63, 144, 16, 16, 64)  # start engines
+        elif mode == States.STOP or mode == States.STOP_BEFORE_EXIT:
+            self.__send_to_drone(126, 63, 64, 63, 144, 16, 16, 160)  # stop
+        elif mode == States.MANUAL_CONTROL:
+            up_down = 126 + int(-1 * my_joystick.get_axis_val(AxisIndex.UP_DOWN) * 126)
+            left_right = 63 + int(my_joystick.get_axis_val(AxisIndex.LEFT_RIGHT) * 63)
+            rotate = 63 + int(my_joystick.get_axis_val(AxisIndex.ROTATE) * 63)
+            forward_backwards = 64 + int(my_joystick.get_axis_val(AxisIndex.FORWARD_BACKWARDS) * 63)
+            self.__send_to_drone(up_down, rotate, forward_backwards, left_right, 144, 16, 16, 0)
 
-def commandCenter(mode, my_joystick):
-    if mode == States.IDLE:
-        send_command(0, 63, 64, 63, 144, 16, 16, 0) # stand by
-    elif mode == States.STAND_BY:
-        send_command(126, 63, 64, 63, 144, 16, 16, 64)  # start engines
-    elif mode == States.STOP or mode == States.STOP_BEFORE_EXIT:
-        send_command(126, 63, 64, 63, 144, 16, 16, 160)  # stop
-    elif mode == States.MANUAL_CONTROL:
-        up_down = 126 + int(-1 * my_joystick.get_axis_val(AxisIndex.UP_DOWN) * 126)
-        left_right = 63 + int(my_joystick.get_axis_val(AxisIndex.LEFT_RIGHT) * 63)
-        rotate = 63 + int(my_joystick.get_axis_val(AxisIndex.ROTATE) * 63)
-        forward_backwards = 64 + int(my_joystick.get_axis_val(AxisIndex.FORWARD_BACKWARDS) * 63)
-        send_command(up_down, rotate, forward_backwards, left_right, 144, 16, 16, 0)
+    @staticmethod
+    def __byte9(mSpeedValue, m360RollValue, mNoHeadValue, mStopValue, mToflyValue, mToLandValue):
+        return mSpeedValue | m360RollValue << 2 | mNoHeadValue << 4 | mStopValue << 5 | mToflyValue << 6 | mToLandValue << 7
