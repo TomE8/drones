070af5176b93538e7bb635c8bc5252928ea62d37
TomE8 <tom.erez8@gmail.com>
2018-09-11 10:14:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import time
+import os
+import cv2
+
+from General.state_machine import getMode
+from General.general_common import Modes
+
+from Control.joystick import Joystick
+from Control.drone_communication import command_center
+
+from Vision.camera import Camera
+
+my_joystick=Joystick(0.1,0.1,0.2,0.1) # TODO: read these values from the config file
+my_camera = Camera()
+mode=Modes.IDLE
+
+while mode != Modes.EXIT:
+    my_joystick.refresh()
+
+    mode=getMode(mode,my_joystick)
+    command_center(mode, my_joystick)
+
+    image = my_camera.get_image()
+    if image is not None:
+        cv2.imshow('Video', image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    time.sleep(0.02)
+
+    print("mode: {}".format(mode))
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    my_joystick.update_vales()
+
+cv2.destroyAllWindows()
+os.system('cls' if os.name == 'nt' else 'clear')
