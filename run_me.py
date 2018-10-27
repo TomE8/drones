4507f98f47071be11221999b300083838d5db146
tom <tom.erez8@gmail.com>
2018-10-27 11:59:26 +0300

    improved exiting the program

--- run_me.py
+++ run_me.py
@@ -5,9 +5,7 @@ import os
 import signal
 import time
 
-pipe = sp.Popen(["./Camera/camera_tcp_session.py"], shell=True, stdout=sp.PIPE, bufsize=10 ** 8,preexec_fn=os.setsid)
-pipe2 = sp.Popen(["./main.py"],  shell=True,stdin=pipe.stdout,preexec_fn=os.setsid)
-time.sleep(1)
-while pipe2.poll() is None: # if pipe2 is still working, go to sleep for 1 second
-    time.sleep(1)
-os.killpg(os.getpgid(pipe.pid), signal.SIGTERM)
+tcp_proc = sp.Popen(["./Camera/camera_tcp_session.py"], shell=True, stdout=sp.PIPE, bufsize=10 ** 8,preexec_fn=os.setsid)
+main_proc = sp.Popen(["./main.py"],  shell=True,stdin=tcp_proc.stdout,preexec_fn=os.setsid)
+main_proc.wait()
+os.killpg(os.getpgid(tcp_proc.pid), signal.SIGTERM)
