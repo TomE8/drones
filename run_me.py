66a1c6027b5e47f57588361f35353d70aad4e956
tom <tom.erez8@gmail.com>
2018-10-27 16:47:33 +0300

    change the apperance

--- run_me.py
+++ run_me.py
@@ -3,9 +3,8 @@
 import subprocess as sp
 import os
 import signal
-import time
 
-tcp_proc = sp.Popen(["./Camera/camera_tcp_session.py"], shell=True, stdout=sp.PIPE, bufsize=10 ** 8,preexec_fn=os.setsid)
-main_proc = sp.Popen(["./main.py"],  shell=True,stdin=tcp_proc.stdout,preexec_fn=os.setsid)
+tcp_proc = sp.Popen(["./Camera/camera_tcp_session.py"], shell=True, stdout=sp.PIPE, bufsize=10 ** 8, preexec_fn=os.setsid)
+main_proc = sp.Popen(["./main.py"],  shell=True, stdin=tcp_proc.stdout, preexec_fn=os.setsid)
 main_proc.wait()
 os.killpg(os.getpgid(tcp_proc.pid), signal.SIGTERM)
